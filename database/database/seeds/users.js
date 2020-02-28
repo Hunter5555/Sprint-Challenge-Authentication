6fb8d237243c792b9e2a2b0c805fab22dc8bdd85
Hunter5555 <hunter.m.allan@gmail.com>
2020-02-28 10:41:17 -0700

    middle

---
+++ database/database/seeds/users.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex) {
+    // Deletes ALL existing entries
+    return knex('users').del()
+      .then(function () {
+        // Inserts seed entries
+        return knex('users').insert([
+          {id: 1, username: 'Drew', password: "test123"},
+          {id: 2, username: 'Vanessa', password: "test123"},
+          {id: 3, username: 'Andy', password: "test123"}
+        ]);
+      });
+  };
